8c8a5574515ed62d942ae666bd9a2f88f4b09e31
furrrmanov <pokerzatashil@mail.ru>
2020-03-13 02:00:42 +0300

    feat: add solution code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,33 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let num1 = expr.match(/\d{1,}/).join('')
+    let num2 = expr.match(/\d{1,}$/g).join('')
+    let br = expr.match(/\((.*)\)/g)
+    let op = expr.match(/[+-/*\/]/g).join('')
+
+    if(num1==0||num2==0&&op=="/"){
+        throw "TypeError: Division by zero."
+    }
+    
+    res = 0
+    
+    switch(op){
+    case "+":  
+        res += +num1 + +num2
+        break;
+    case "-":  
+        res += +num1 - +num2
+        break;
+    case "*":  
+        res += +num1 * +num2
+        break;
+    case "/":  
+        res += +num1 / +num2
+        break;
+        
+    }
+    return res
 }
 
 module.exports = {
